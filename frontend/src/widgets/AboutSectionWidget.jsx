3dc16bb40e03f3ca36e3ae75be5b1237f01fbcf2
Carlos Amaral <72256470+carlosabdoamaral@users.noreply.github.com>
2022-11-20 18:33:48 -0300

    removing website

--- frontend/src/widgets/AboutSectionWidget.jsx
+++
@@ -1,9 +0,0 @@
-import { data } from "../static/data/data"
-
-export const AboutSectionWidget = _ => {
-    return (
-        <div className="about-section-widget section-widget">
-            <p>{data.about}</p>
-        </div>
-    )
-}
